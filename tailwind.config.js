9cbd104471464d7d071534d43acdba86f6eccaee
Matt Engerer <matt.engerer@aol.com>
2022-07-14 16:55:25 -0400

    more styling changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,45 @@ module.exports = {
       backgroundImage: {
         'bg-1': "url('../japas.png')",
         'bg-2': "url('../ramen-bg.avif')",
-      }
-      
-    },
+      },
+      keyframes: {
+        // 'wiggle': {
+        //   '0%, 100%': { transform: 'rotate(-3deg)' },
+        //   '50%': { transform: 'rotate(3deg)' },
+        // }, 
 
+'fadeindown': {
+  
+  '0%': {
+    opacity: '0',
+    transform: 'translateY(-10px)'
+},
+'100%': {
+    opacity: '1',
+    transform: 'translateY(0)'
+}
+  },
+
+  'fadeinup': {
+  
+    '0%': {
+      opacity: '0',
+      transform: 'translateY(10px)'
+  },
+  '100%': {
+      opacity: '1',
+      transform: 'translateY(0)'
+  }
+    }
+
+
+
+      },
+      animation: {
+        'fadeindown': 'fadeindown 0.5s ease-out',
+        'fadeinup': 'fadeindown 0.6s ease-out'
+    }
+    }, 
   },
   variants: {
     extend: {},
